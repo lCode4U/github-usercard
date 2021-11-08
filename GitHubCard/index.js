3e141d6bfe4d83d6046b677f1ab81dcf46439701
Torquoya-Jackson <tlwilliams92tech@yahoo.com>
2021-11-07 20:53:53 -0800

    hhh

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,31 +1,42 @@
+<<<<<<< Updated upstream
 import axios from 'axios';
+=======
+>>>>>>> Stashed changes
 
+import { render } from 'react-dom';
+import axios from 'axios';
+import react, { useState } from 'react';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-// const gitHubData = fetch("https://api.github.com/users/ICode4U");
-// console.log(gitHubData);
-// gitHubData.then(Response => console.log(Response));
-// gitHubData.catch(error => console.log(error));
-// gitHubData.finally(() =>  {
-//   const userData = useState(gitHubData);
-//   console.log(userData);
-// }); 
+// const gitHubData = axios.get("https://api.github.com/users/ICode4U");
 
 
+
+
+ 
+
+// //console.log(gitHubData);
+// //gitHubData.then(gitHubData => (toString))
+// // gitHubData.catch(error => console.log(error));
+
+  
 
 const gitRequest = axios.get('https://api.github.com/users/ICode4U');
 
 
 
   gitRequest.then(gitRequest => {
-    console.log(gitRequest.data);
+    return gitRequest.toString;
   });
   gitRequest.catch(gitProblem => console.log(gitProblem));
 
-
+//gitRequest.finally(gitRequest => userData => {
+  //gitRequest.prototype.toString(Response)
+  //return userData;
+//});
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -71,6 +82,7 @@ const followersArray = [];
       </div>
     </div>
 */
+<<<<<<< Updated upstream
 // function GitComponent(props) {
 //   return (
 //     <div className="card">
@@ -89,6 +101,29 @@ const followersArray = [];
 //   </div>
 //   )
 // }
+=======
+export function GitComponent(props) {
+  
+  const userData = useState(gitRequest)
+  
+  return (
+    <div className="card">
+    <img src={props.gitImage} />
+    <div className="card-info">
+      <h3 className="name">{props.userName}</h3>
+      <p className="username">{props.screenName}</p>
+      <p>Location: {props.usersLocation}</p>
+      <p>Profile:
+        <a href={props.githubAdd}>{props.githubPage}</a>
+      </p>
+      <p>Followers: {props.usersFollowers}</p>
+      <p>Following: {props.usersFollowing}</p>
+      <p>Bio: {props.usersBio}</p>
+    </div>
+  </div>
+  )
+}
+>>>>>>> Stashed changes
 // 
 // render(
 //   <GitComponent  gitImage='' userName=''   screenName=''  userLocation='' githubAdd='' githubPage='' usersFollowers='' userFollowings='' usersBio=''/>
